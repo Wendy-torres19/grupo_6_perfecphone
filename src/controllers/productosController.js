1442d56a7882220ce88f472d4e28eb766e50fead
elias <moriancumer6@gmail.com>
2021-07-20 00:06:36 -0300

    proyecto enlasado

--- src/controllers/productosController.js
+++ src/controllers/productosController.js
@@ -1,5 +1,5 @@
 const path = require("path");
 
 module.exports = {
-    productos: (req,res)=> res.sendFile(path.resolve(__dirname,"../views","productos.html")),    
+    productos: (req,res)=> res.render("productos"),    
 }
