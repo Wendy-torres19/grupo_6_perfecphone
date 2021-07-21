f5f1a9cbec34c7bf9c40fe54ade134c8a4f6b8e2
elias <moriancumer6@gmail.com>
2021-07-20 23:56:11 -0300

    actualizacion del proyecto

---
+++ src/controllers/formularioUsuario.js
@@ -0,0 +1,5 @@
+const path = require("path");
+
+module.exports = {
+    formularioUsuario: (req,res)=> res.render("formularioUsuario"), 
+}
--- src/routes/main.js
+++ src/routes/main.js
@@ -2,10 +2,13 @@ const express = require("express");
 const router = express.Router();
 const main = require("../controllers/main");
 const productos = require("../controllers/productosController");
+const formularioUsuario = require("../controllers/formularioUsuario");
 
 router.get("/",main.index);
 router.get("/about",main.index);
 
 router.get("/productos",productos.productos);
+
+router.get("/formularioUsuario",formularioUsuario.formularioUsuario);
 
 module.exports = router;
